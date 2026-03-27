498a4f9c0a448abaa93039da67c3fffb946919b0
Durga <skdurga2218@gmail.com>
2026-03-27 23:18:30 +0530

    fixed deployment port

--- app.py
+++ app.py
@@ -769,4 +769,12 @@ if __name__ == '__main__':
     with app.app_context():
         if not os.path.exists(app.config['DATABASE']):
             init_db()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+
+if __name__ == "__main__":
+    with app.app_context():
+        if not os.path.exists(app.config['DATABASE']):
+            init_db()
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
